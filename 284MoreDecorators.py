8619a8a4cab6d8b9738e791e235ba9341741787b
0buz <54356021+0buz@users.noreply.github.com>
2019-08-21 17:54:30 +0100

    Add files via upload

--- 284MoreDecorators.py
+++ 284MoreDecorators.py
@@ -49,3 +49,85 @@ def add_all(*nums):
 
 print("Sum is ",add_all(1,8))
 
+
+#==============================================================================================================
+def only_ints(fn):
+    @wraps(fn)
+    def wrapper(*args,**kwargs):
+        """Only execute if arguments are integer."""
+        # for arg in args:
+        #     if type(arg) != int:
+        #         raise TypeError("Please only invoke with integers.")
+
+        #the above can be written using "any" + list comprehension
+        if any([arg for arg in args if type(arg) != int]):
+             raise TypeError("Please only invoke with integers.")
+        return fn(*args,**kwargs)
+    return wrapper
+
+@only_ints
+def add(x,y):
+    return x+y
+
+print(add(6,4))
+#==============================================================================================================
+
+#==============================================================================================================
+def ensure_authorized(fn):
+    @wraps(fn)
+    def wrapper(*args,**kwargs):
+        """Only execute if keyword argument exists as role = "admin"."""
+        if kwargs.get('role')=='admin':
+            return fn(*args,**kwargs)
+        return "Unauthorised"
+    return wrapper
+
+@ensure_authorized
+def access(**dct):
+    return "Authorised"
+
+print(access(role="admin"))
+#==============================================================================================================
+
+#==============================================================================================================
+
+#passing argument to decorator; we need another layer; see "inner" below
+def ensure_first_arg_is(val):
+    def inner(fn):
+        @wraps(fn)
+        def wrapper(*args,**kwargs):
+           """Only execute if the first argument is."""
+           if args and args[0] != val:   # if args exist and the first one is not equal to val
+                raise ValueError(f"First arg needs to be {val}.")
+           return fn(*args,**kwargs)
+        return wrapper
+    return inner
+
+@ensure_first_arg_is(7)
+def add_to_seven(a,b):
+    return a+b
+
+print("Sum is",add_to_seven(7,8))
+#==============================================================================================================
+
+
+#==============================================================================================================
+from time import sleep
+
+def delay(countdown):
+    def inner(fn):
+        @wraps(fn)
+        def wrapper(*args,**kwargs):
+            """Execute after waiting x many seconds."""
+            print(f"Waiting {countdown} seconds before running function.")
+            sleep(countdown)
+            return fn(*args,**kwargs)
+        return wrapper
+    return inner
+
+@delay(3)
+def say_hi():
+    return "Hi"
+
+print(say_hi())   
+#==============================================================================================================
