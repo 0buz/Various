85805732d2b086f158ff6cdf06f33dfa602a51a7
0buz <ady_peiu@yahoo.com>
2020-02-19 13:15:03 +0000

    var assignment

--- DataEngineering/postgresIntro.py
+++ DataEngineering/postgresIntro.py
@@ -57,6 +57,7 @@ cur.execute("SELECT typname FROM pg_catalog.pg_type WHERE oid=25 or oid=700;")
 type_name_25 = cur.fetchone()[0]
 type_name_700 = cur.fetchone()[0]   # fetch the next row
 all_results = cur.fetchall()
+type_name_25 = cur.fetchone()[1]
 #=====================================================================================================================
 
 
