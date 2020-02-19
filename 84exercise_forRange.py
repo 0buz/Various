f29debf810eb33dea2daf8cc723fed6c74c4b710
0buz <ady_peiu@yahoo.com>
2020-02-19 12:24:47 +0000

    reorganise

--- 84exercise_forRange.py
+++
@@ -1,9 +0,0 @@
-# Add up all odd numbers between 10 and 20
-# Store the result in x:
-x = 0
-
-# YOUR CODE GOES HERE:
-for nbr in range(10,20):
-    if nbr%2 != 0:
-        x += nbr
-print("Odd number sum is {}.".format(x))
--- sup.py
+++
@@ -1,6 +0,0 @@
-#course slide 223
-
-def say_sup():
-    print(f"Sup, my __name__ is {__name__}.")
-
-say_sup()
